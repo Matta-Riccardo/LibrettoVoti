46ac1dcbc33dacada5fbaacc09084dc1c25f0af6
riccardo <riccardo@LAPTOP-28JRDE4L>
2022-04-03 11:27:36 +0200

    prima sol

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,25 +1,94 @@
 package it.polito.tdp.librettovoti;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.librettovoti.model.Libretto;
+import it.polito.tdp.librettovoti.model.Voto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
+public class FXMLController {
+	
+	private Libretto model; 
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<Integer> cmbPunti;     //al posto di <?> devo inserire la classe che voglio, e quindi l'insieme di valori possibili e li vado a definire sotto
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextArea txtVoti;
     
     @FXML
-    private Label label;
-    
+    private Label txtStatus;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void handleNuovoVoto(ActionEvent event) {      //quando premo il bottone dovrei chiamare il metodo add della classe Libretto (il Model di questa applicazione)
+    	
+    	//1. acquisizione e controllo dati
+    	String nome = txtNome.getText();
+    	Integer punti = cmbPunti.getValue();
+    	
+    	//controlli di validità
+    	if(nome.equals("") || punti==null) {
+    		//errore, non posso eseguire l'operazione
+    		txtStatus.setText("ERRORE: occorre inserire nome e voto\n");
+    		return;
+    	}
+    	
+    	//2. esecuzione dell'operazione (== chiedere al Model di farla)
+    	boolean ok = model.add(new Voto(nome, punti));
+    	
+    	//3.visualizzazione/aggiornamento del risultato
+    	
+    	//String contenutoLibretto = model.toString();  //qui è il model a costruire una stringa che passa al controller, in realtà io vorrei che si parlassero a oggetti, aggiungerò un metodo in Libretto
+    	//txtVoti.setText(contenutoLibretto);
+    	if (ok) {
+    		List<Voto> voti = model.getVoti();
+    		txtVoti.appendText("Hai superato " +voti.size()+ " esami\n");
+    		for(Voto v: voti) {
+    			txtVoti.appendText(v.toString()+"\n");
+    		}
+    	
+    	
+    	txtNome.clear();
+    	cmbPunti.setValue(null);
+    	txtStatus.setText("");
+    	
+    	}else {
+    		txtStatus.setText("ERRORE: esame già presente");
+    	}
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Libretto model) {
+    	this.model = model;
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbPunti != null : "fx:id=\"cmbPunti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtVoti != null : "fx:id=\"txtVoti\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        cmbPunti.getItems().clear(); //si comporta come una lista, e in quanto tale possiede tutti i metodi della lista (per sicurezza la pulisco)
+        for(int p = 18; p <= 30; p++) {
+        	cmbPunti.getItems().add(p);
+        }
+
+    }
+
 }
+
---
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -0,0 +1,111 @@
+package it.polito.tdp.librettovoti.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Libretto {
+
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>(); //qua definisco l'interfaccia che gestisce la lista, inizialmente non conterrà nessun voto
+		
+	}
+	
+	public boolean add(Voto v) {
+		//this.voti.add(v); //uso il metodo add della lista per aggiungere in coda un elemento della stessa classe (delego alla List l'operazione che dovrei fare io)
+	    if(!isDuplicate(v) && !isConflitto(v)) {
+	    	this.voti.add(v);
+	    	return true;
+	    }else {
+	    	return false;
+	    }
+	}
+	
+	//creo un metodo che restituisce un nuovo libretto filtrando sul voto
+	public Libretto filtraPunti(int punti) {
+		Libretto result = new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getPunti()==punti) {
+				result.add(v);
+				}
+		}
+		return result;
+	}
+	
+	//implemento la documentazione del metodo:
+	/**
+	 * Restituisce il punteggio ottenuto all'esame di cui
+	 * specifico il nome
+	 * @param nome Nome dell'esame
+	 * @return punteggio numerico, oppure null se l'esame non esiste
+	 */
+	
+	public Integer puntiEsame(String nome) {
+		for(Voto v: this.voti) {
+			if(v.getNome().equals(nome)) {
+				return v.getPunti();
+			}
+		}
+		//return -1; restituendo interi il -1 potrà poi essere usado erronaamente come un volore lecitp abbassandomi la media
+		
+		return null; //(ricordando di restituire un Integer e non un int normale)
+		//posso usare anche un'eccezione 
+		//throw new IllegalArgumentException("Corso non trovato");
+	}
+	
+	public boolean isDuplicate(Voto v) {
+		for (Voto v1: this.voti) {
+			if(v1.equals(v))
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean isConflitto(Voto v) {
+		Integer punti = this.puntiEsame(v.getNome());
+		if (punti!= null && punti!=v.getPunti())
+			return true;
+		else
+			return  false;
+	}
+	
+	public List<Voto> getVoti(){
+		return this.voti;
+	}
+	
+	public Libretto votiMigliorati(){
+		Libretto nuovo = new Libretto();
+		for(Voto v: voti) {
+			int punti =v.getPunti();
+			if(punti >= 24) {
+				punti +=2;
+			}else {
+				punti ++;
+			}
+			
+			if(punti > 30) {
+				punti = 30;
+			}
+			
+			//v.setPunti(punti);
+			//nuovo.add(v);     così non va bene sennò cambierei anche nell'altro libretto il punteggio(cambieri l'oggetto), avrei due liste che puntano allo stesso oggetto
+			
+			nuovo.add(new Voto(v.getNome(), punti));
+		}
+		return nuovo;
+	}
+	
+	public void cancellaVotiMinori(int punti) {
+		//come cancello i voti dalla lista? (quale remove?)
+		for(Voto v: this.voti) {
+			if(v.getPunti()<punti) {  //però questo metodo non funziona
+				this.voti.remove(v);
+			}
+		}
+	}
+	
+	public String toString() {
+		return this.voti.toString(); //delego alla list il compito di stamparsi
+	}
+}
